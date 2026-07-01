c022ba632f9c07c3ed1a794645aaa1be42ed20dd
Advait-Sen <advaitsen78@gmail.com>
2026-07-01 20:48:47 +0200

    Using new config

--- src/main/java/adsen/helium/Helium.java
+++ src/main/java/adsen/helium/Helium.java
@@ -1,7 +1,46 @@
 package adsen.helium;
 
+import adsen.helium.arguments.HeliumConfig;
+import adsen.helium.parser.Parser;
+import adsen.helium.tokeniser.Tokeniser;
+
 public class Helium {
-    static void main() {
-        System.out.println("Welcome to Helium!");
+    static HeliumConfig config;
+
+    static void main(String[] args) {
+        config = HeliumConfig.parse(args);
+
+        printlnVerbose("Welcome to Helium!");
+
+        printlnVerbose();
+        printlnVerbose("Setting up tokeniser:");
+        Tokeniser tokeniser = new Tokeniser(config);
+
+        // tokeniser.tokenise()
+
+        if (config.parse()) {
+            printlnVerbose();
+            printlnVerbose("Setting up parser:");
+            Parser parser = new Parser(config);
+
+            // parser.parse()
+        }
+    }
+
+    public static void printlnVerbose(Object... args) {
+        if (config.verbose()) {
+            for (Object arg : args) {
+                System.out.print(arg);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printVerbose(Object... args) {
+        if (config.verbose()) {
+            for (Object arg : args) {
+                System.out.print(arg);
+            }
+        }
     }
 }
